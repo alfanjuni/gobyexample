9c6c4cc4d9d1c2f247d15990e691d8e7228f617e
alfanjuni <alfanjuni@gmail.com>
2024-10-20 23:40:30 +0700

    feat: Added examples for basic structure keywords

---
+++ examples/golang-25-magic-keywords/01-package/README.md
@@ -0,0 +1,7 @@
+# package keyword
+
+The `package` keyword defines the package that a file belongs to. Go organizes source code into packages. 
+
+Example:
+```go
+package main
---
+++ examples/golang-25-magic-keywords/01-package/package.go
@@ -0,0 +1,10 @@
+// This file demonstrates the use of the 'package' keyword.
+// Every Go file belongs to a package, which helps organize code into namespaces.
+package main
+
+import "fmt"
+
+// main is the entry point of a Go program.
+func main() {
+	fmt.Println("Hello from the main package!")
+}
